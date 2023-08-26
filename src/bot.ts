d859891965f26f5e6b5f30a5460bd45908ab8ee3
sashawhosaid <sashawhosaid@gmail.com>
2023-08-27 02:05:39 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -570,7 +570,7 @@ async function deleteAllMessages(chatId: number, message_id: number) {
       console.log("currMessageId:",currMessageId);
       await bot.api.deleteMessage(chatId, currMessageId);
       currMessageId=currMessageId-1;
-    }catch(err){console.log("error deleteMessage: ", err);break;}
+    }catch(err){console.log("error deleteMessage: ", err)}
   }
     console.log(`All messages in the chat ${chatId} have been deleted.`);
 }
