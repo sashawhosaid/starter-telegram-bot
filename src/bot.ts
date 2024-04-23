69c1553ca6b8d201e63678150336b9dd12b6bec7
sashawhosaid <sashawhosaid@gmail.com>
2024-04-24 01:47:59 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -567,12 +567,12 @@ bot.on("message", async (ctx) =>{
       let maxLabel:string='';
       result.forEach(async (Classification,index)=>{
         const {label,score}= Classification
-        await ctx.reply(Label+" "+ score)
+        await ctx.reply(label+" "+ score)
         //const parsedScore :number = parseInt(score.toString(),10);
         //if (parsedScore>max){
         //  max=parsedScore;
         //  maxLabel=label;
-        }
+       // }
       })
       await ctx.reply(maxLabel+" - "+ (max*100) +' %')
 
