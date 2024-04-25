3e531783dbd61aff212d625cf525c0b371858bb9
sashawhosaid <sashawhosaid@gmail.com>
2024-04-25 20:23:51 +0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -642,7 +642,7 @@ if (process.env.NODE_ENV === "production") {
   // Use Webhooks for the production server
   const app = express();
   app.use(express.json());
-  //app.use(webhookCallback(bot, "express"));
+  app.use(webhookCallback(bot, "express"));
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log(`Bot listening on port ${PORT}`);
